5947739a489c1568894cbeb8a4c0afcacf56dd98
xbmc-addon <xbmc.addon@gmail.com>
2013-08-29 14:55:01 +0400

    add cache for vk - v.0.0.8

--- src/core/api.py
+++ src/core/api.py
@@ -7,7 +7,8 @@ from lastfm import LastFM, LastFMError
 from vk import VK, VKError
 from fanarttv import FanartTV, FanartTVError
 
-cache  = xbmcup.db.Cache(xbmcup.system.fs('sandbox://lastvk.cache.db'))
-lastfm = LastFM('2c207878e17c021c6ee060f8f39487f2', '570aa4c51ccb896a4130363ca55ecac2', 'plugin.audio.lastvk', 'lastfm_login', 'lastfm_password', 'lastfm_session_key')
-vk     = VK('3819266', 'vk_login', 'vk_password', 'vk_token')
-fanart = FanartTV('8f02185df376a8ec860a83cdeb41ca23')
+cache   = xbmcup.db.Cache(xbmcup.system.fs('sandbox://lastvk.cache.db'))
+cacheVK = xbmcup.db.Cache(xbmcup.system.fs('sandbox://vk.cache.db'))
+lastfm  = LastFM('2c207878e17c021c6ee060f8f39487f2', '570aa4c51ccb896a4130363ca55ecac2', 'plugin.audio.lastvk', 'lastfm_login', 'lastfm_password', 'lastfm_session_key')
+vk      = VK('3819266', 'vk_login', 'vk_password', 'vk_token')
+fanart  = FanartTV('8f02185df376a8ec860a83cdeb41ca23')
--- src/core/play.py
+++ src/core/play.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import urllib
+
 import xbmcup.app
 
 from google import YouTube
-from api import vk
+from api import vk, cacheVK
 
 
 class PlayVideo(xbmcup.app.Handler):
@@ -18,13 +20,20 @@ class PlayAudio(xbmcup.app.Handler):
             data = [dict(url=self.argv['url'], title=self.argv['title'], artist=self.argv['artist'], duration=self.argv['duration'])]
 
         else:
-            result = vk.api('audio.search', q=u' - '.join([self.argv['artist'], self.argv['song']]), auto_complete=1, count=300)
-            if not result or not result['items']:
-                return None
-
-            data = self.filter_one(result['items'])
+            token = str(':'.join([urllib.quote(self.argv['artist'].lower().encode('utf8')), urllib.quote(self.argv['song'].lower().encode('utf8'))]))
+            
+            data = self._get_cache(token)
             if not data:
-                return None
+
+                result = vk.api('audio.search', q=u' - '.join([self.argv['artist'], self.argv['song']]), auto_complete=1, count=300)
+                if not result or not result['items']:
+                    return None
+
+                data = self.filter(result['items'])
+                if not data:
+                    return None
+                
+                cacheVK.set(token, '_'.join([str(data[0]['owner_id']), str(data[0]['id'])]))
 
 
         self.parent.path = data[0]['url']
@@ -35,7 +44,22 @@ class PlayAudio(xbmcup.app.Handler):
         return self.parent
 
 
+    def filter(self, data):
+        return self.filter_one(data)
+
+
     def filter_one(self, data):
         artist = self.argv['artist'].lower()
         song = self.argv['song'].lower()
         return [x for x in data if x['artist'].lower() == artist and x['title'].lower() == song]
+
+
+    def _get_cache(self, token):
+        aid = cacheVK.get(token)[token]
+        if not aid:
+            return None
+        
+        result = vk.api('audio.getById', audios=aid)
+        if result:
+            return result[0:1]
+        
