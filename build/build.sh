509385b41066cadf65023856e4f22f497c49a133
maxkhvorov <maxmang@gmail.com>
2014-12-25 01:42:48 +0300

    Move to Kodi

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
 cd `dirname $0`/../src
-cp -r ./* $HOME/Library/Application\ Support/XBMC/addons/plugin.audio.lastvk/
+cp -r ./* $HOME/Library/Application\ Support/Kodi/addons/plugin.audio.lastvk/
 
-rm -f $HOME/.xbmc/temp/xbmcup/plugin.audio.lastvk/lastvk.cache.db
+rm -f $HOME/.kodi/temp/xbmcup/plugin.audio.lastvk/lastvk.cache.db
